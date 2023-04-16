3d2a7a08f96f54050e56664ef9b3d80f30003ef4
Keshav Garg <kggargkg@gmail.com>
2023-04-16 11:31:34 +0530

    updated featuresconstants json filw

--- src/sections/Features/featuresConstant.js
+++ src/sections/Features/featuresConstant.js
@@ -9,30 +9,30 @@ export const Info = [
         "name" : "Doctors",
         "description" : "Consult with our doctors for any type of consultaion you need related to your health",
         "image" : "./img/logog.svg",
-        "Link" : "exercise",
+        "Link" : "doctors",
     },
     {
         "name" : "Community",
         "description" : "Join the community and you can ask any questions related to your health in the community anonymously",
         "image" : "./img/community.svg",
-        "Link" : "",
+        "Link" : "community",
     },
     {
         "name" : "Quiz",
         "description" : "Attempt a quiz and get to know about the consition of your periods and recommended exercises for you",
         "image" : "./img/quiz.png",
-        "Link" : "",
+        "Link" : "quiz",
     },
     {
         "name" : "Pregnancy",
         "description" : "Pregnancies might be overwhelming and scary at the same time. Click to know everything about pregnancy.",
         "image" : "./img/preg.png",
-        "Link" : "",
+        "Link" : "pregnancy",
     },
     {
         "name" : "Women's Health",
         "description" : "Here's to all the women out there!Here's to how you can lead a better and healthy Lifestyle",
         "image" : "./img/wo.png",
-        "Link" : "",
+        "Link" : "health",
     }
 ];
