9c55bbb6e8ff7ac3c44671447da2e2a7353ce829
Keshav Garg <kggargkg@gmail.com>
2023-04-16 11:46:01 +0530

    added doctors and updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,15 +5,26 @@ function Navbar() {
   return (
     <header className="text-white bg-pink-400 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link to='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link
+          to="/"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
           <img src="./img/logog.svg" className="w-11 h-11 text-white"></img>
           <span className="ml-3 text-xl text-white">HerCycle</span>
         </Link>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-white	flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">Doctor</a>
-          <a className="mr-5 hover:text-gray-900">Exercises</a>
-          <a className="mr-5 hover:text-gray-900">Blog</a>
-          <a className="mr-5 hover:text-gray-900">Community</a>
+          <Link to="doctors" className="mr-5 hover:text-gray-900">
+            Doctor
+          </Link>
+          <Link to="exercises" className="mr-5 hover:text-gray-900">
+            Exercises
+          </Link>
+          <Link to="blog" className="mr-5 hover:text-gray-900">
+            Blog
+          </Link>
+          <Link to="community" className="mr-5 hover:text-gray-900">
+            Community
+          </Link>
         </nav>
         <button className="inline-flex items-center text-pink-600 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           login
