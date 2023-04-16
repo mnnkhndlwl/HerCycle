3d5b0d5207b854cf68626e74ec202fb46a3e152e
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-04-16 12:27:37 +0530

    exercises complete

--- src/pages/Exercises/Exercises.jsx
+++ src/pages/Exercises/Exercises.jsx
@@ -15,7 +15,7 @@ function Exercises() {
           </p>
         </div>
         <section class="text-gray-600 body-font">
-          <div class="container mx-auto flex px-5 md:flex-row flex-col items-center">
+          <div class="container mx-auto flex px-5 my-10 md:flex-row flex-col items-center">
             <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                 Cobra pose
@@ -35,7 +35,7 @@ function Exercises() {
             </div>
             <div class="lg:max-w-lg lg:w-full md:w-1/2 mr-10 w-5/6">
               <iframe
-              className="object-cover object-center rounded"
+                className="object-cover object-center rounded"
                 width="560"
                 height="315"
                 src="https://www.youtube.com/embed/CxI6DjNafyw"
@@ -44,6 +44,101 @@ function Exercises() {
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen
               ></iframe>
+            </div>
+          </div>
+          <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+              <iframe
+                className="object-cover object-center rounded"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/M1gEGLtF1p0"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Pigeon pose
+              </h1>
+              <p class="mb-8 leading-relaxed">
+                First, position yourself in the upright sitting position. Bend
+                your right knee, and extend your left leg behind you. Arch your
+                back as you place your hands on your hips. A more intense
+                stretch can be achieved if you reach your arms over your head
+                and bring your hands together. Position yourself on all fours.
+                Bring your right knee forward towards your right wrist, and
+                extend your right ankle so it is in line with your left hip.
+                Slowly slide your leg back. For balance and depending on your
+                flexibility, you may need to keep your hands positioned on the
+                ground. As your leg extends back, you will feel the stretching
+                of your left hip and you may also feel stretching along your
+                right side, but it shouldn't be painful. If it is painful, you
+                may need to modify the pose. Hold this pose for several breaths
+                before returning to the center and repeating the pose with your
+                right leg outstretched behind you and your left knee bent.
+                Repeat 5 to 10 times.
+              </p>
+            </div>
+          </div>
+          <div class="container mx-auto flex px-5 my-10 md:flex-row flex-col items-center">
+            <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Squats
+              </h1>
+              <p class="mb-8 leading-relaxed">
+                stand with feet a little wider than hip width, toes facing
+                front. Drive your hips back—bending at the knees and ankles and
+                pressing your knees slightly open—as you… Sit into a squat
+                position while still keeping your heels and toes on the ground,
+                chest up and shoulders back.Strive to eventually reach parallel,
+                meaning knees are bent to a 90-degree angle.Press into your
+                heels and straighten legs to return to a standing upright
+                position.
+              </p>
+            </div>
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 mr-10 w-5/6">
+              <iframe
+                className="object-cover object-center rounded"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/otzWCWpuW-A"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+          <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+              <iframe
+                className="object-cover object-center rounded"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/QOVaHwm-Q6U"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+            <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Lunges
+              </h1>
+              <p class="mb-8 leading-relaxed">
+                Start in a standing position with your feet hip-width apart.
+                Step forward longer than a walking stride so one leg is ahead of
+                your torso and the other is behind. Your foot should land flat
+                and remain flat while it’s on the ground. Your rear heel will
+                rise off of the ground. Bend your knees to approximately 90
+                degrees as you lower yourself. Remember to keep your trunk
+                upright and core engaged. Then, forcefully push off from your
+                front leg to return to the starting position.
+              </p>
             </div>
           </div>
         </section>
