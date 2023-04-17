4faf83fe7c5fb629aedae6048735a3c2e4b79f7d
Keshav Garg <kggargkg@gmail.com>
2023-04-17 14:47:25 +0530

    bug fix for check cycle

--- src/pages/check/Check.jsx
+++ src/pages/check/Check.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Calendar } from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React, { useState } from "react";
+import { Calendar } from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function Check() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [cycleLength, setCycleLength] = useState(28);
-  const [periodLength, setPeriodLength] = useState(5);
+  var [selectedDate, setSelectedDate] = useState(new Date());
+  var [cycleLength, setCycleLength] = useState(28);
+  var [periodLength, setPeriodLength] = useState(5);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -20,19 +20,28 @@ function Check() {
   };
 
   const calculateNextPeriodDate = () => {
-    const ovulationDate = new Date(selectedDate.getTime() + ((cycleLength - 14) * 24 * 60 * 60 * 1000));
-    const nextPeriodDate = new Date(ovulationDate.getTime() + ((periodLength + 2) * 24 * 60 * 60 * 1000));
+    const ovulationDate = new Date(
+      selectedDate.getTime() + (cycleLength - 14) * 24 * 60 * 60 * 1000
+    );
+    const nextPeriodDate = new Date(
+      ovulationDate.getTime() + periodLength * 24 * 60 * 60 * 1000
+    );
     return nextPeriodDate;
   };
 
   const nextPeriodDate = calculateNextPeriodDate();
 
   const calendarTileContent = ({ date, view }) => {
-    if (view === 'month') {
+    if (view === "month") {
       if (date.toDateString() === nextPeriodDate.toDateString()) {
-        return <div className="bg-red-500 rounded-full h-6 w-6 mx-auto"></div>; 
-      }
-      else if (date >= nextPeriodDate && date <= new Date(nextPeriodDate.getTime() + ((periodLength - 1) * 24 * 60 * 60 * 1000))) {
+        return <div className="bg-red-500 rounded-full h-6 w-6 mx-auto"></div>;
+      } else if (
+        date >= nextPeriodDate &&
+        date <=
+          new Date(
+            nextPeriodDate.getTime() + (periodLength - 1) * 24 * 60 * 60 * 1000
+          )
+      ) {
         return <div className="bg-red-500 rounded-full h-6 w-6 mx-auto"></div>;
       }
       //  else if (date > nextPeriodDate && date < new Date(nextPeriodDate.getTime() + (cycleLength * 24 * 60 * 60 * 1000))) {
@@ -43,25 +52,49 @@ function Check() {
 
   return (
     <>
-      
-
       <div className="max-w-screen-lg py-28 mx-auto mt-8">
         <h1 className="text-3xl font-bold mb-4">Check Your Next Period</h1>
 
         <div className="flex justify-between">
           <div className="w-1/2 pr-4">
-            <label htmlFor="cycle-length" className="block font-medium mb-2">Cycle Length (in days)</label>
-            <input type="number" id="cycle-length" name="cycle-length" value={cycleLength} onChange={handleCycleLengthChange} className="w-full p-2 rounded border border-gray-400 mb-4" />
+            <label htmlFor="cycle-length" className="block font-medium mb-2">
+              Cycle Length (in days)
+            </label>
+            <input
+              type="number"
+              id="cycle-length"
+              name="cycle-length"
+              value={cycleLength}
+              onChange={handleCycleLengthChange}
+              className="w-full p-2 rounded border border-gray-400 mb-4"
+            />
 
-            <label htmlFor="period-length" className="block font-medium mb-2">Period Length (in days)</label>
-            <input type="number" id="period-length" name="period-length" value={periodLength} onChange={handlePeriodLengthChange} className="w-full p-2 rounded border border-gray-400 mb-4" />
+            <label htmlFor="period-length" className="block font-medium mb-2">
+              Period Length (in days)
+            </label>
+            <input
+              type="number"
+              id="period-length"
+              name="period-length"
+              value={periodLength}
+              onChange={handlePeriodLengthChange}
+              className="w-full p-2 rounded border border-gray-400 mb-4"
+            />
 
-            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Check</button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+              Check
+            </button>
           </div>
 
           <div className="w-1/2 pl-4">
-            <label className="block font-medium mb-2">Select the first day of your last period</label>
-            <Calendar value={selectedDate} onChange={handleDateChange} tileContent={calendarTileContent} />
+            <label className="block font-medium mb-2">
+              Select the first day of your last period
+            </label>
+            <Calendar
+              value={selectedDate}
+              onChange={handleDateChange}
+              tileContent={calendarTileContent}
+            />
           </div>
         </div>
 
