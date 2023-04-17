4ac7469ca56e3faeba6836dc15ff9063c7c98949
Keshav Garg <kggargkg@gmail.com>
2023-04-17 14:34:06 +0530

    added pregnancy page updated hero

--- src/pages/Pregnancy/Pregnancy.jsx
+++ src/pages/Pregnancy/Pregnancy.jsx
@@ -2,7 +2,341 @@ import React from "react";
 
 function Pregnancy() {
   return (
-    <h1>Pregnancy</h1>
+    <div>
+      <div className="flex items-center text-center p-2 justify-center mt-16 text-2xl">
+        <a
+          href="#health"
+          className="px-9 py-4 mr-10 text-white no-underline bg-pink-300 rounded-3xl hover:bg-pink-500 hover:no-underline hover:text-black"
+        >
+          Preganancy Health
+        </a>
+        <a
+          href="#lifestyle"
+          className="px-6 py-4 ml-10 text-white no-underline bg-pink-300 rounded-3xl hover:bg-pink-500 hover:no-underline hover:text-black"
+        >
+          Preganancy Lifestyle
+        </a>
+      </div>
+      <div id="health">
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-6 mt-10 mx-auto flex flex-wrap">
+            <div class="flex flex-wrap -m-4 mx-auto">
+              <div class="p-4 lg:w-1/2 md:w-full mx-full">
+                <div class="flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col p-1/2 py-14">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      alt=""
+                      src="https://flo.health/uploads/media/sulu-846x-inset/08/7858-molar-pregnancy-01_846x635.jpg?v=1-0"
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Complications
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Molar pregnancy: What it is and how it feels
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-health/complications/molar-pregnancy"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/2 md:w-full mx-full">
+                <div class="flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col p-1/2 py-11">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-ful flex-shrink-0">
+                    <img
+                      alt=""
+                      src="https://flo.health/uploads/media/sulu-846x-inset/01/7841-miscarriage-vs-period-01_846x635.jpg?v=1-0"
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Pregnancy Health
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      How to tell the difference between a miscarriage and a
+                      period
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-health/miscarriage-vs-period"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container px-5 py-6 mx-auto flex flex-wrap">
+            <div class="flex flex-wrap -m-4 mx-auto">
+              <div class="p-4 lg:w-1/2 md:w-full mx-full">
+                <div class="flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col p-1/2 py-11">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      src="https://flo.health/uploads/media/sulu-846x-inset/02/7852-pregnancy-after-miscarriage-01_846x635.jpg?v=1-0"
+                      alt=""
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Miscarriage
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Pregnancy after miscarriage: Chances, symptoms, and
+                      success stories
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-health/pregnancy-loss/pregnancy-after-miscarriage"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/2 md:w-full mx-full">
+                <div class="flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col p-1/2 py-14">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      src="https://flo.health/uploads/media/sulu-846x-inset/04/8314-Pregnant%20woman%20feeling%20quickening%2001_846x635.png?v=1-0"
+                      alt=""
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Pregnancy Health
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Quickening and fetal movement: First baby kicks explained
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-health/quickening"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div id="lifestyle" className="mb-10">
+        <hr className="h-2 rounded-2xl bg-pink-500 mx-96 my-10" />
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-6 mx-auto flex flex-wrap">
+            <div class="flex flex-wrap -m-4 mx-auto">
+              <div class="p-4 lg:w-1/2 md:w-full">
+                <div class="flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col py-11">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      alt=""
+                      src="https://flo.health/uploads/media/sulu-846x-inset/05/7195-Drinks%20for%20pregnant%20women%2001_846x635.jpg?v=1-0"
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Diet
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Drinks for pregnant women: What can you drink while
+                      pregnant, and what should you avoid?
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-lifestyle/nutrition-and-weight/drink-during-pregnancy"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/2 md:w-full">
+                <div class="flex border-2 rounded-lg border-gray-300 border-opacity-50 p-8 sm:flex-row flex-col py-14">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      src="https://flo.health/uploads/media/sulu-846x-inset/06/7206-Pregnancy%20nutrition%2002_846x635.jpg?v=1-0"
+                      alt=""
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Diet
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Healthy pregnancy diet: What food is and isn't safe to eat
+                      during pregnancy?
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-lifestyle/nutrition-and-weight/healthy-pregnancy-diet"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container px-5 py-6 mx-auto flex flex-wrap">
+            <div class="flex flex-wrap -m-4 mx-auto">
+              <div class="p-4 lg:w-1/2 md:w-full">
+                <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col py-11">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      src="https://flo.health/uploads/media/sulu-846x-inset/00/7220-Heartburn%20during%20pregnancy%2001_846x635.jpg?v=1-0"
+                      alt=""
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Diet
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      The common causes and remedies of heartburn during
+                      pregnancy
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-lifestyle/nutrition-and-weight/heartburn-during-pregnancy"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/2 md:w-full">
+                <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                  <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <img
+                      src="https://flo.health/uploads/media/sulu-846x-inset/06/7066-Pregnancy%20cravings%2002_846x635.jpg?v=1-0"
+                      alt=""
+                      className="rounded-2xl object-contain"
+                    />
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Diet
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      9 most common pregnancy cravings: When they start and what
+                      to do about them
+                    </p>
+                    <a
+                      class="mt-3 text-pink-500 inline-flex items-center"
+                      href="https://flo.health/pregnancy/pregnancy-lifestyle/nutrition-and-weight/pregnancy-cravings"
+                    >
+                      Learn More
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   );
 }
 
