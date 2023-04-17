db4acbd2b5cfc5ab47ff6233b62a34487ae0b216
Keshav Garg <kggargkg@gmail.com>
2023-04-17 21:39:22 +0530

    added profile page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -63,24 +63,33 @@ function Navbar() {
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
             </button>
+            <Link to="profile" className="ml-8 hover:text-gray-900">
+              <img
+                src="https://img.icons8.com/fluency/48/null/test-account.png"
+                alt="profile"
+                title="Profile"
+              />
+            </Link>
           </>
         ) : (
-          <Link to="login">
-            <button className="inline-flex items-center text-pink-600 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-              login
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-4 h-4 ml-1"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </button>
-          </Link>
+          <>
+            <Link to="login">
+              <button className="inline-flex items-center text-pink-600 bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+                login
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 ml-1"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </button>
+            </Link>
+          </>
         )}
       </div>
     </header>
